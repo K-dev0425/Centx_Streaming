66fd19c7fb3aefacba5b44013fb036f98db3c50a
aibek-dev <aibek-dev@outlook.com>
2021-06-05 23:30:05 +0500

    test

--- includes/classes/user.class.php
+++ includes/classes/user.class.php
@@ -3174,7 +3174,8 @@ class userquery extends CBCategory{
                 lang('user_change_pass')	=>'edit_account.php?mode=change_password',
                 lang('user_change_email') 	=>'edit_account.php?mode=change_email',
                 lang('com_manage_subs')	=> 'edit_account.php?mode=subscriptions',
-                lang('contacts_manager')	=> 'manage_contacts.php'
+                lang('contacts_manager')	=> 'manage_contacts.php',
+                lang('report')	=> 'account_report.php',
             );
 
 
